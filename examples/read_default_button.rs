6b2ac229c5409592d6741752692ae3066d5cbab2
Richard Neumann <neumann@paulmann.de>
2024-09-12 10:26:55 +0200

    Implement base listener

--- examples/read_default_button.rs
+++ examples/read_default_button.rs
@@ -1,14 +1,17 @@
-use smik_buttons::Button;
+use evdev::Device;
+use smik_buttons::listen;
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
 
 const BUTTON: &str = "/dev/input/by-path/platform-gpio-keys-event";
 
 fn main() {
-    for event in Button::new(BUTTON.into())
-        .listen()
-        .expect("Failed to listen to button events")
-    {
-        if let Some(event) = event {
-            println!("{event:?}");
-        }
+    let events = listen(
+        Device::open(BUTTON).expect("Failed to open button device"),
+        Arc::new(AtomicBool::new(true)),
+    );
+
+    for event in events {
+        println!("Read event: {event:?}");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
-mod button;
+use evdev::{Device, InputEvent};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{channel, Receiver};
+use std::sync::Arc;
+use std::thread::spawn;
 
-pub use button::Button;
+/// Listen for input events from a device.
+///
+/// # Panics
+/// This function may panic if it fails to send an event.
+pub fn listen(mut device: Device, running: Arc<AtomicBool>) -> Receiver<InputEvent> {
+    let (tx, rx) = channel();
+
+    spawn(move || {
+        while running.load(Ordering::Relaxed) {
+            if let Ok(events) = device.fetch_events() {
+                for event in events {
+                    tx.send(event).expect("Failed to send event");
+                }
+            }
+        }
+    });
+
+    rx
+}
