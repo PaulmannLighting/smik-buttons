8e2748adb17cfc75f69cd5d355674bd2e8123508
Richard Neumann <neumann@paulmann.de>
2024-09-12 14:23:35 +0200

    Add clap as argument parser

--- examples/read_default_button.rs
+++ examples/read_default_button.rs
@@ -1,12 +1,21 @@
+use clap::Parser;
 use evdev::Device;
 use smik_buttons::Events;
 
 const BUTTON: &str = "/dev/input/by-path/platform-gpio-keys-event";
 
+#[derive(Debug, Parser)]
+struct Args {
+    /// Path to the button device.
+    #[clap(long, default_value = BUTTON)]
+    button: String,
+}
+
 fn main() {
     env_logger::init();
+    let args = Args::parse();
 
-    for event in Events::from(Device::open(BUTTON).expect("Failed to open button device")) {
+    for event in Events::from(Device::open(args.button).expect("Failed to open button device")) {
         println!("Read event: {event:?}");
     }
 }
