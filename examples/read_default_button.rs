1517e391eedfd584f218bba0c79f2ec033fe0354
Richard Neumann <neumann@paulmann.de>
2024-09-12 10:56:02 +0200

    Introduce Listener struct

--- examples/read_default_button.rs
+++ examples/read_default_button.rs
@@ -1,15 +1,13 @@
 use evdev::Device;
-use smik_buttons::listen;
-use std::sync::atomic::AtomicBool;
-use std::sync::Arc;
+use smik_buttons::Listener;
 
 const BUTTON: &str = "/dev/input/by-path/platform-gpio-keys-event";
 
 fn main() {
-    for event in listen(
-        Device::open(BUTTON).expect("Failed to open button device"),
-        Arc::new(AtomicBool::new(true)),
-    ) {
+    let (_listener, events) =
+        Listener::spawn(Device::open(BUTTON).expect("Failed to open button device"));
+
+    for event in events {
         println!("Read event: {event:?}");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,51 @@
 use evdev::{Device, InputEvent};
+use std::sync::mpsc::Sender;
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     mpsc::{channel, Receiver},
     Arc,
 };
-use std::thread::spawn;
+use std::thread::{spawn, JoinHandle};
 
-/// Listen for input events from a device.
-///
-/// # Panics
-/// This function may panic if it fails to send an event.
-pub fn listen(mut device: Device, running: Arc<AtomicBool>) -> Receiver<InputEvent> {
-    let (tx, rx) = channel();
+#[derive(Debug)]
+pub struct Listener {
+    running: Arc<AtomicBool>,
+    thread: Option<JoinHandle<()>>,
+}
 
-    spawn(move || {
-        while running.load(Ordering::Relaxed) {
-            if let Ok(events) = device.fetch_events() {
-                for event in events {
-                    tx.send(event).expect("Failed to send event");
+impl Listener {
+    /// Listen for input events from a device.
+    ///
+    /// # Panics
+    /// This function may panic if it fails to send an event.
+    #[must_use]
+    pub fn spawn(device: Device) -> (Self, Receiver<InputEvent>) {
+        let (tx, rx) = channel();
+        (Self::new(Arc::new(AtomicBool::new(true)), device, tx), rx)
+    }
+
+    fn new(running: Arc<AtomicBool>, mut device: Device, tx: Sender<InputEvent>) -> Self {
+        Self {
+            running: running.clone(),
+            thread: Some(spawn(move || {
+                while running.load(Ordering::Relaxed) {
+                    if let Ok(events) = device.fetch_events() {
+                        for event in events {
+                            tx.send(event).expect("Failed to send event");
+                        }
+                    }
                 }
-            }
+            })),
         }
-    });
+    }
+}
 
-    rx
+impl Drop for Listener {
+    fn drop(&mut self) {
+        self.running.store(false, Ordering::Relaxed);
+
+        if let Some(thread) = self.thread.take() {
+            thread.join().expect("Failed to join listener thread");
+        }
+    }
 }
