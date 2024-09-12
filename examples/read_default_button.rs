51c315e24036e1b3507214afaaac5bd88d57e6f3
Richard Neumann <neumann@paulmann.de>
2024-09-12 14:20:43 +0200

    Add env_logger to dev dependencies

--- examples/read_default_button.rs
+++ examples/read_default_button.rs
@@ -4,6 +4,8 @@ use smik_buttons::Events;
 const BUTTON: &str = "/dev/input/by-path/platform-gpio-keys-event";
 
 fn main() {
+    env_logger::init();
+
     for event in Events::from(Device::open(BUTTON).expect("Failed to open button device")) {
         println!("Read event: {event:?}");
     }
